0f0b0c25b0d1461b5a16eab5d40f4d8db87a0023
es-lee <dldms800@gmail.com>
2015-09-15 16:29:21 +0900

    5번도 함. 아마도 숙제 다 함

--- hw1/hw1_2_grade.ml
+++ hw1/hw1_2_grade.ml
@@ -1,9 +1,14 @@
 open CommonGrade
 open Hw1_2
 
-let foo n = n * 2
+let foo = fun n -> n * 2
+let bar = fun n -> n * n
 
-let _ = check (fun () -> (sigma (1, 10, foo) = 110))
-let _ = check (fun () -> (sigma (5, 10, foo) = 90))
-let _ = check (fun () -> (sigma (1, 100, foo) = 10100))
-let _ = check (fun () -> (sigma (8, 8, foo) = 16))
+let _ = check (fun () -> sigma (3, 1, foo) = 0)
+let _ = check (fun () -> sigma (4, 2, bar) = 0)
+let _ = check (fun () -> sigma (8, 8, foo) = 16)
+let _ = check (fun () -> sigma (3, 4, bar) = 25)
+let _ = check (fun () -> sigma (1, 10, foo) = 110)
+let _ = check (fun () -> sigma (1, 10, bar) = 5 * 7 * 11)
+let _ = check (fun () -> sigma (5, 10, foo) = 90)
+let _ = check (fun () -> sigma (1, 100, foo) = 10100)
--- hw1/hw1_3_grade.ml
+++ hw1/hw1_3_grade.ml
@@ -7,4 +7,5 @@ let f2 = fun x -> fun y -> x * y
 let _ = check (fun () -> (iter (10, f1)) 3 = 23)
 let _ = check (fun () -> (iter (2, f1)) 121 = f1 (f1 121))
 let _ = check (fun () -> (iter (3, f1)) 177 = f1 (f1 (f1 177)))
-let _ = check (fun () -> ((iter (2, f2 2)) 4 = f2 2 (f2 2 4)))
+let _ = check (fun () -> (iter (4, f2 1)) 44 = 44)
+let _ = check (fun () -> ((iter (2, f2 7)) 4 = f2 7 (f2 7 4)))
