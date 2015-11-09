b3bd80c6da1983e647dfaa914b6a3886e7e0184b
es-lee <dldms800@gmail.com>
2015-11-09 13:58:27 +0900

    var x trasnlated.

--- hw5/SM5_skel/translate.ml
+++ hw5/SM5_skel/translate.ml
@@ -14,7 +14,7 @@ module Translator = struct
     | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
     | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
     | K.UNIT -> [Sm5.PUSH (Sm5.Val (Sm5.Unit))]
-    | K.VAR i -> failwith "Unimplemented"
+    | K.VAR i -> [Sm5.PUSH (Sm5.Id i)] @ [Sm5.LOAD]
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
     | K.SUB (e1, e2) -> trans e1 @ trans e2 @ [Sm5.SUB]
     | K.MUL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.MUL]
