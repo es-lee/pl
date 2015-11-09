44641480fb1107d0f037955a20b06dd3b10bb5d8
es-lee <dldms800@gmail.com>
2015-11-09 15:39:31 +0900

    write implemented

--- hw5/SM5_skel/translate.ml
+++ hw5/SM5_skel/translate.ml
@@ -14,7 +14,7 @@ module Translator = struct
     | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
     | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
     | K.UNIT -> [Sm5.PUSH (Sm5.Val (Sm5.Unit))]
-    | K.VAR i -> [Sm5.PUSH (Sm5.Id i)] @ [Sm5.LOAD]
+    | K.VAR i -> [Sm5.PUSH (Sm5.Id i); Sm5.LOAD]
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
     | K.SUB (e1, e2) -> trans e1 @ trans e2 @ [Sm5.SUB]
     | K.MUL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.MUL]
@@ -22,10 +22,10 @@ module Translator = struct
     | K.EQUAL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.EQ]
     | K.LESS (e1, e2) -> trans e1 @ trans e2 @ [Sm5.LESS]
     | K.NOT e -> trans e @ [Sm5.NOT]
-    | K.ASSIGN (id, exp) -> failwith "Unimplemented"
-    | K.SEQ (e1, e2)-> failwith "Unimplemented"
-    | K.IF (e1, e2, e3) -> failwith "Unimplemented"
-    | K.WHILE (e1, e2) -> failwith "Unimplemented"
+    | K.ASSIGN (id, exp) -> trans exp @ [Sm5.PUSH (Sm5.Id id); Sm5.STORE; Sm5.PUSH (Sm5.Id id); Sm5.LOAD]
+    | K.SEQ (e1, e2)-> trans e1 @ trans e2
+    | K.IF (e1, e2, e3) -> trans e1 @ [Sm5.JTR (trans e2, trans e3)]
+    | K.WHILE (e1, e2) -> trans e1 @ failwith "Unimplemented"
     | K.FOR (id, e1, e2, e3) -> failwith "Unimplemented"
     | K.LETV (x, e1, e2) ->
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
@@ -34,6 +34,8 @@ module Translator = struct
     | K.CALLV (id, exp) -> failwith "Unimplemented"
     | K.CALLR (id1, id2)-> failwith "Unimplemented"
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
-    | K.WRITE e -> failwith "Unimplemented"
+    | K.WRITE e -> trans e @ [Sm5.MALLOC; Sm5.BIND "α"; Sm5.PUSH (Sm5.Id "α"); Sm5.Store;
+                              Sm5.PUSH (Sm5.Id "α"); Sm5.PUSH (Sm5.Id "α"); Sm5.Load;
+                              Sm5.PUT; Sm5.Load; Sm5.UNBIND; Sm5.POP]
     | _ -> failwith "Unimplemented"
 end
